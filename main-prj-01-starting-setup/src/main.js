105ed104cc903113fddd99751cdece7980c082b8
iiwal <iiwally26@gmail.com>
2025-07-26 15:34:27 -0500

    fa

--- main-prj-01-starting-setup/src/main.js
+++ main-prj-01-starting-setup/src/main.js
@@ -1,3 +1,19 @@
 import { createApp } from 'vue';
 
-createApp({}).mount('#app');
+import App from "./App.vue";
+import store from "./store/index.js";
+import router from "./router.js";
+import BaseCard from "@/components/ui/BaseCard.vue";
+import BaseButton from "@/components/ui/BaseButton.vue";
+import BaseBadge from "@/components/ui/BaseBadge.vue";
+
+app.component("BaseCard", BaseCard);
+app.component("BaseBadge", BaseBadge);
+app.component("BaseButton", BaseButton);
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+
+app.mount('#app');
---
+++ main-prj-01-starting-setup/src/router.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachDetail from "./pages/coaches/CoachDetail.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+import UserRequests from "./pages/user/UserRequests.vue";
+import NotFound from "./components/NotFound.vue";
+import ContactCoach from "./pages/user/ContactCoach.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/coaches' },
+        { path: '/coaches', component: CoachesList},
+        { path: '/coaches/:id', component: CoachDetail, children: [
+                { path: 'contact', component: ContactCoach}
+            ]},
+        { path: '/register', component: CoachRegistration },
+        { path: '/requests', component: UserRequests},
+        { path: '/:notFound(.*)', component: NotFound},
+    ]
+});
+
+export default router;
